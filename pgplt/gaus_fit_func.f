47218c68d790703454c8395a27c1b318030c9bf4
jdh <jdh>
2008-02-04 18:14:49 +0000

    Changed to allow improved Gauss-fitting: extra parameter added

--- pgplt/gaus_fit_func.f
+++ pgplt/gaus_fit_func.f
@@ -1,14 +1,15 @@
 !
-! Program to evaluate a multiple Gaussian fit to a set of
+! Program to evaluate a multiple pseudo-Gaussian fit to a set of
 ! data points passed by the module GAUS_FIT_DATA.
 !
 ! The function has the form
 !
-!   Y =P1 + P2*(X-X(1))+ P5*EXP( -((X-P3)/P4)^2) + ... 
+!   Y =P1 + P2*(X-X(1))+ P5*EXP( -((X-P3)/P4)^P6) + ... 
 !
 ! The routine returns the fit (YFIT) and the SQUARED error.
 ! between the fit and data.
 !
+! Altered   -Sep-2007 : Use of alternative exponent to 2 installed.
 ! Created 21-Jul-2005
 !
 	REAL*8 FUNCTION GAUS_FIT_FUNC(PARAMS)
@@ -16,14 +17,14 @@
 	IMPLICIT NONE
 	REAL*8 PARAMS(NG_PAR)
 !
-	REAL SUM
+	REAL*8 SUM
 	INTEGER I,J,K
 !
 	GAUS_FIT_FUNC=0.0D0
 	DO J=1,NG_DATA
 	  SUM=PARAMS(1)+PARAMS(2)*(X_GAUS(J)-X_GAUS(1))
-	  DO K=3,NG_PAR,3
-	   SUM=SUM+PARAMS(K+2)*EXP(-((X_GAUS(J)-PARAMS(K))/PARAMS(K+1))**2)
+	  DO K=3,NG_PAR,4
+	   SUM=SUM+PARAMS(K+2)*EXP(-(ABS((X_GAUS(J)-PARAMS(K))/PARAMS(K+1)))**PARAMS(K+3))
 	  END DO
 	  YFIT(J)=SUM
 	  GAUS_FIT_FUNC=GAUS_FIT_FUNC+(Y_GAUS(J)-SUM)**2
