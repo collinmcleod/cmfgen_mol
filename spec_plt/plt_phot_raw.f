26e3455cf69ddacb7c9acb949412f618964f6417
jdh <jdh>
2008-04-17 19:31:16 +0000

    Can now read in level energies from file

--- spec_plt/plt_phot_raw.f
+++ spec_plt/plt_phot_raw.f
@@ -7,6 +7,10 @@
 	USE GEN_IN_INTERFACE
 	IMPLICIT NONE
 !
+! Altered 16-Apr-2008 : Read in energy levels form oscilator file (if available).
+!                       Deleted log plot section (since can be done in pgplot)
+!                       Can now eneter level index for name. If unrecognized level,
+!                          all names are dumped to terminal.
 ! Altered 15-Jan-2008 : TYPE_1 etc changed to allocatable arrays.
 !                       Errors returned if files cannot be opened.
 ! Created 09-Jun-1999
@@ -19,6 +23,8 @@
 	CHARACTER(LEN=30), ALLOCATABLE :: NAME_1(:)
 	REAL*8, ALLOCATABLE :: NU_1(:)
 	REAL*8, ALLOCATABLE :: CROSS_1(:)
+	REAL*8, ALLOCATABLE :: ENERGY_1(:)
+	REAL*8, ALLOCATABLE :: STAT_WT_1(:)
 !
 	INTEGER NPAIRS_2
 	INTEGER NLEV_2
@@ -28,6 +34,14 @@
 	CHARACTER(LEN=30), ALLOCATABLE :: NAME_2(:)
 	REAL*8, ALLOCATABLE :: NU_2(:)
 	REAL*8, ALLOCATABLE :: CROSS_2(:)
+!
+! These are used to read the Oscilator file.
+!
+	INTEGER NELEV
+	REAL*8, ALLOCATABLE :: ENERGY(:)
+	REAL*8, ALLOCATABLE :: G(:)
+	REAL*8 IONIZATION_ENERGY
+	CHARACTER(LEN=30), ALLOCATABLE :: E_NAME(:)
 !
 	CHARACTER*30 LEVEL_NAME1
 	CHARACTER*30 LEVEL_NAME2
@@ -41,11 +55,17 @@
 !
 	REAL*8 XV(10000),YV(10000)
 !
-	REAL*8 STAT_WT,GION,EDGE,TOTAL_REC,TEMP
+	REAL*8 STAT_WEIGHT,GION,EDGE,TOTAL_REC,TEMP
+	REAL*8 ANG_TO_HZ,SPEED_OF_LIGHT
+	REAL*8, PARAMETER :: RONE=1.0D0
+	LOGICAL DO_RECOM
+	EXTERNAL SPEED_OF_LIGHT
+!
+	ANG_TO_HZ=1.0D-07*SPEED_OF_LIGHT()
 !
 	WRITE(6,'(A)')' '
 	WRITE(6,'(A)')'Program designed to plot/compare opacity project data from photoionization files '
-	WRITE(6,'(A)')'Use DISPGEN to plot no tabulated cross-sections '
+	WRITE(6,'(A)')'Use DISPGEN to plot tabulated cross-sections '
 	WRITE(6,'(A)')' '
 !
 10	FILENAME='PHOT1'
@@ -66,7 +86,8 @@
 	    END IF
 	    IF( INDEX(STRING,'!Number of energy levels') .NE. 0)THEN
 	      READ(STRING,*)NLEV_1
-	      ALLOCATE (TYPE_1(NLEV_1),NUM_VALS_1(NLEV_1),LOC_1(NLEV_1), NAME_1(NLEV_1),STAT=IOS)
+	      ALLOCATE (TYPE_1(NLEV_1),NUM_VALS_1(NLEV_1),LOC_1(NLEV_1),
+	1               NAME_1(NLEV_1),ENERGY_1(NLEV_1),STAT_WT_1(NLEV_1),STAT=IOS)
 	      IF(IOS .NE. 0)THEN
 	        WRITE(6,*)'Error in PLT_PHOT_RAW -- unable to allocate storage (1)'
 	        WRITE(6,*)'STAT=',IOS
@@ -103,6 +124,82 @@
 	    STRING=' '
 	  END DO
 	CLOSE(UNIT=10)
+!
+	I=INDEX(FILENAME,'_A')
+	IF(I .EQ.  0)I=INDEX(FILENAME,'_B')
+	IF(I .GT. 6)THEN
+	  FILENAME=FILENAME(5:I)//'F_OSCDAT'	
+	ELSE
+	  FILENAME=' '
+	END IF
+!
+! Read in energy levels from oscilator file, if it is available.
+! May need to change if oscilator file format changes.
+!
+14	CONTINUE
+	CALL GEN_IN(FILENAME,'File with oscillator data: "" to type in indivdual level energies')
+	IF(FILENAME .NE. " ")THEN
+	  OPEN(UNIT=10,STATUS='OLD',FILE=FILENAME,IOSTAT=IOS)
+	  IF(IOS .NE. 0)THEN
+	    WRITE(6,*)'Unable to open file: ',TRIM(FILENAME)
+	    WRITE(6,*)'IOSTAT=',IOS
+	    GOTO 14
+	  END IF
+	  STRING=' '
+!	  DO WHILE(INDEX(STRING,'!Format date') .EQ. 0)
+!	    READ(10,'(A)')STRING
+!	  END DO
+!	  IF(INDEX(STRING,'17-Oct-2000') .EQ. 0)THEN
+!	    WRITE(6,*)'Oscilator format date is not recognized by this routine'
+!	    GOTO 14
+!	  END IF
+	  DO WHILE(INDEX(STRING,'!Number of energy levels') .EQ. 0)
+	    READ(10,'(A)')STRING
+	  END DO
+	  READ(STRING,*)NELEV
+	  DO WHILE(INDEX(STRING,'!Ionization energy') .EQ. 0)
+	    READ(10,'(A)')STRING
+	  END DO
+	  READ(STRING,*)IONIZATION_ENERGY
+	  DO WHILE(INDEX(STRING,'!Number of transition') .EQ. 0)
+	    READ(10,'(A)')STRING
+	  END DO
+	  READ(10,'(A)')STRING		!Get blank line
+	  ALLOCATE(G(NELEV),E_NAME(NELEV),ENERGY(NELEV))
+!
+	  G(:)=0.0D0;ENERGY(:)=0.0D0
+	  DO I=1,NELEV
+	    READ(10,'(A)')STRING
+	    K=INDEX(STRING,'  ')
+	    E_NAME(I)=STRING(1:K-1)
+	    READ(STRING(K:),*)G(I),ENERGY(I)
+	    IF(E_NAME(I)(K-1:K-1) .EQ.  ']')THEN
+	      K=INDEX(E_NAME(I),'[')
+	      E_NAME(I)(K:)=' '
+	    END IF
+	    WRITE(6,*)G(I),ENERGY(I)
+	  END DO
+	  CLOSE(UNIT=10)
+	  PAUSE
+!
+! Now need to match names. We assume photoionzation files do not
+! have [].
+!
+	  ENERGY_1(1:NLEV_1)=0.0D0; STAT_WT_1(1:NLEV_1)=0.0D0
+	  DO I=1,NLEV_1
+	    DO J=1,NELEV
+	      IF(E_NAME(J) .EQ. NAME_1(I))THEN
+	        ENERGY_1(I)=ENERGY_1(I)+G(J)*ENERGY(J)
+	        STAT_WT_1(I)=STAT_WT_1(I)+G(J)
+	      END IF
+	    END DO
+	    IF(STAT_WT_1(I) .NE. 0.0D0)ENERGY_1(I)=1.0D-15*SPEED_OF_LIGHT()*
+	1        (IONIZATION_ENERGY-ENERGY_1(I)/STAT_WT_1(I))
+	  END DO
+	ELSE
+	  ENERGY_1(1:NLEV_1)=0.0D0; STAT_WT_1(1:NLEV_1)=0.0D0
+	END IF
+	WRITE(6,*)ENERGY_1(1),STAT_WT_1(1)
 !
 !
 !
@@ -168,17 +265,29 @@
 !
 	DO WHILE(1 .EQ. 1)
 !
+! We can now use a level name, or an index (sas stored in PHOT file).
+!
 	  INDX_1=0
+	  LEVEL_NAME1='1'
 	  DO WHILE(INDX_1 .EQ. 0)
-	    CALL GEN_IN(LEVEL_NAME1,'Level name for File 1')
+	    CALL GEN_IN(LEVEL_NAME1,'Level name [or index] for File 1')
 	    IF(LEVEL_NAME1 .EQ. ' ')STOP
-            DO J=1,NLEV_1
+	    DO J=1,NLEV_1
 	      IF(LEVEL_NAME1 .EQ. NAME_1(J))THEN
                 INDX_1=J
                 EXIT
 	      END IF
 	    END DO
-	    IF(INDX_1 .EQ. 0)WRITE(6,*)'Error - level name not found'
+	    IF(INDX_1 .EQ. 0)THEN
+	      READ(LEVEL_NAME1,*,IOSTAT=IOS)INDX_1
+	      IF(IOS .NE. 0)THEN
+	        WRITE(6,*)'Error - level name not found'
+	        DO I=1,NLEV_1,5
+	          WRITE(6,'(5A14)')(TRIM(NAME_1(J)),J=I,MAX(I+4,NLEV_1))
+	        END DO
+	        INDX_1=0
+	      END IF
+	    END IF
 	  END DO
 	  WRITE(6,*)'Level_1 name is: ',NAME_1(INDX_1)
 !
@@ -187,18 +296,26 @@
 	  YV(1:NV)=CROSS_1(LOC_1(INDX_1):LOC_1(INDX_1)+NV-1)
 	  CALL DP_CURVE(NV,XV,YV)
 !
-	  STAT_WT=1
+	  STAT_WEIGHT=MAX(RONE,STAT_WT_1(INDX_1))
+	  EDGE=ENERGY_1(INDX_1)
 	  GION=1 
-	  EDGE=0
-	  WRITE(6,'(A)')' '
-	  WRITE(6,'(A)')'A non-zero ionization energy is only required to compute'
-	  WRITE(6,'(A)')'the recombination rate.'
-	  WRITE(6,'(A)')' '
-	  CALL GEN_IN(EDGE,'Ionization energy (10^15 Hz)')
-	  IF(EDGE .NE. 0)THEN
+	  IF(EDGE .EQ. 0.0D0)THEN
+	    WRITE(6,'(A)')' '
+	    WRITE(6,'(A)')'A non-zero ionization energy is only required to compute'
+	    WRITE(6,'(A)')'the recombination rate.'
+	    WRITE(6,'(A)')' '
+	    CALL GEN_IN(EDGE,'Ionization energy (10^15 Hz)')
+	    DO_RECOM=.TRUE.
+	    IF(EDGE .EQ. 0)DO_RECOM=.FALSE.
+	  ELSE
+	    WRITE(6,*)'Energy of level is',EDGE
+	    DO_RECOM=.FALSE.
+	    CALL GEN_IN(DO_RECOM,'Compute recombination rate?')
+	  END IF
+	  IF(DO_RECOM)THEN
 	    CALL GEN_IN(TEMP,'Temperature in 10^4K')
 	    IF(TEMP .NE. 0)THEN
-	      CALL RECOM_OPAC(YV,XV,EDGE,STAT_WT,GION,NV,NV,TOTAL_REC,TEMP)
+	      CALL RECOM_OPAC(YV,XV,EDGE,STAT_WEIGHT,GION,NV,NV,TOTAL_REC,TEMP)
 	      WRITE(6,*)'Total Rec=',TOTAL_REC
 	    END IF
 	  END IF
@@ -220,37 +337,24 @@
 	    XV(1:NV)=NU_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1)
 	    YV(1:NV)=CROSS_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1)
 	    CALL DP_CURVE(NV,XV,YV)
-	    IF(EDGE .NE. 0)THEN
+	    IF(DO_RECOM)THEN
 	      IF(TEMP .NE. 0)THEN
-	        CALL RECOM_OPAC(YV,XV,EDGE,STAT_WT,GION,NV,NV,TOTAL_REC,TEMP)
+	        CALL RECOM_OPAC(YV,XV,EDGE,STAT_WEIGHT,GION,NV,NV,TOTAL_REC,TEMP)
 	        WRITE(6,*)'Total Rec=',TOTAL_REC
 	      END IF
 	    END IF
 	  END IF
 	  CALL GRAMON_PGPLOT('\gn/\gn\do\u ','\gs(Mb)',LEVEL_NAME1,' ')
 !
-	  WRITE(6,*)'Now doing a LOG plot'
-	  NV=NUM_VALS_1(INDX_1)
-	  XV(1:NV)=LOG10(NU_1(LOC_1(INDX_1):LOC_1(INDX_1)+NV-1))
-	  YV(1:NV)=LOG10(CROSS_1(LOC_1(INDX_1):LOC_1(INDX_1)+NV-1)+1.0D-100)
-	  CALL DP_CURVE(NV,XV,YV)
-	  IF(NPAIRS_2 .NE. 0)THEN
-	    NV=NUM_VALS_2(INDX_2)
-	    XV(1:NV)=DLOG10(NU_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1))
-	    YV(1:NV)=DLOG10(CROSS_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1)+1.0D-100)
-	    CALL DP_CURVE(NV,XV,YV)
-	  END IF
-	  CALL GRAMON_PGPLOT('Log \gn/\gn\do\u ','Log \gs(Mb)',LEVEL_NAME1,' ')
-!
-	  IF(EDGE .NE. 0)THEN
+	  IF(EDGE .NE. 0.0D0 .AND. XV(1) .NE. 0.0D0)THEN
 	    WRITE(6,*)'Now doing wavelength plot'
 	    NV=NUM_VALS_1(INDX_1)
-	    XV(1:NV)=0.299794D+04/(NU_1(LOC_1(INDX_1):LOC_1(INDX_1)+NV-1))/EDGE
+	    XV(1:NV)=ANG_TO_HZ/(NU_1(LOC_1(INDX_1):LOC_1(INDX_1)+NV-1))/EDGE
 	    YV(1:NV)=CROSS_1(LOC_1(INDX_1):LOC_1(INDX_1)+NV-1)
 	    CALL DP_CURVE(NV,XV,YV)
 	    IF(NPAIRS_2 .NE. 0)THEN
 	      NV=NUM_VALS_2(INDX_2)
-	      XV(1:NV)=0.299794D+04/(NU_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1))/EDGE
+	      XV(1:NV)=ANG_TO_HZ/(NU_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1))/EDGE
 	      YV(1:NV)=CROSS_2(LOC_2(INDX_2):LOC_2(INDX_2)+NV-1)
 	      CALL DP_CURVE(NV,XV,YV)
 	    END IF
