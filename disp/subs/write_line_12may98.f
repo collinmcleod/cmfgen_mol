d9cf71d6a310e769e6524f1606f571f7380b2686
jdh <jdh>
2017-12-06 18:52:22 +0000

    Primarily adding files that were not in CVS

---
+++ disp/subs/write_line_12may98.f
@@ -0,0 +1,71 @@
+	SUBROUTINE WRITE_LINE_12MAY98(TRAN_NAME,MOD_NAME,
+	1             DIF,CONT_INT,FREQ,AMASS,R,V,SIGMA,T,
+	1             ETA,CHI,ESEC,CHIL,ETAL,ND,LUOUT)
+	IMPLICIT NONE
+!
+	CHARACTER(LEN=*) TRAN_NAME
+	CHARACTER(LEN=*) MOD_NAME
+!
+	INTEGER ND
+	INTEGER LUOUT 
+!
+	REAL*8 R(ND)
+	REAL*8 V(ND)
+	REAL*8 SIGMA(ND)
+	REAL*8 T(ND)
+	REAL*8 ETA(ND)
+	REAL*8 CHI(ND)
+	REAL*8 ESEC(ND)
+	REAL*8 ETAL(ND)
+	REAL*8 CHIL(ND)
+!
+	LOGICAL DIF
+	REAL*8 CONT_INT
+	REAL*8 FREQ
+	REAL*8 AMASS
+!
+        REAL*8 VAC_LAM
+        REAL*8 SPEED_OF_LIGHT
+        EXTERNAL SPEED_OF_LIGHT
+!
+	INTEGER I
+	CHARACTER(LEN=20) TMP_STR
+	I=LEN_TRIM(MOD_NAME)
+	TMP_STR=MOD_NAME
+!
+	VAC_LAM=1.0D-07*SPEED_OF_LIGHT()/FREQ
+!
+	IF(TRAN_NAME .EQ. 'Continuum')THEN
+	  IF(I .GT. 20)TMP_STR=MOD_NAME(I-19:I)
+   	  WRITE(LUOUT,'(1X,A,T25,A,T40,A)')'12-May-1998','[Date]','Revised format date'
+   	  WRITE(LUOUT,'(1X,A,T25,A,T40,A)')TRIM(TMP_STR),'[MOD_ID]','Model'
+	  WRITE(LUOUT,'(1X,A,T25,A,T40,A)')'Continuum:','[TR_ID]','Transition identification'
+ 	  WRITE(LUOUT,'(1X,A,T25,A,T40,A)')DIF,'[DIF]','Diffusion approximation'
+	  WRITE(LUOUT,'(1X,1PE15.8,T25,A,T40,A)')FREQ,'[FREQ]','Frequency (10^15 Hz)'
+	  WRITE(LUOUT,'(1X,1PE12.5,T25,A,T40,A)')VAC_LAM,'[LAM]','Wavelength (Ang)(vacuum)'
+	  WRITE(LUOUT,'(1X,1PE15.8,T25,A,T40,A)')CONT_INT,'[IC]','Schuster intensity'
+	  WRITE(TMP_STR,'(I5)')ND; TMP_STR=ADJUSTL(TMP_STR)
+          WRITE(LUOUT,'(1X,A,T25,A,T40,A)')TRIM(TMP_STR),'[ND]','Number of depth points'
+
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'R',(R(I),I=1,ND)
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'T',(T(I),I=1,ND)
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'SIGMA',(SIGMA(I),I=1,ND)
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'V',(V(I),I=1,ND)
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'ETA',(ETA(I),I=1,ND)
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'CHI_TH',( (CHI(I)-ESEC(I)) ,I=1,ND)
+	  WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'ESEC',(ESEC(I),I=1,ND)
+!
+	ELSE
+          WRITE(LUOUT,'(1X,A,T25,A,T40,A)')TRIM(TRAN_NAME),'[TR_ID]','Transition identification'
+          WRITE(LUOUT,'(1X,1PE15.8,T25,A,T40,A)')FREQ,'[FREQ]','Frequency (10^15 Hz)'
+          WRITE(LUOUT,'(1X,1PE12.5,T25,A,T40,A)')VAC_LAM,'[LAM]','Wavelength (Ang)(vacuum)'
+          WRITE(LUOUT,'(1X,F6.2,T25,A,T40,A)')AMASS,'[AMASS]','Atomic mass'
+	  WRITE(LUOUT,'(1X,1PE15.8,T25,A,T40,A)')CONT_INT,'[IC]','Schuster intensity'
+          WRITE(LUOUT,*)' '
+          WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'ETAL',(ETAL(I),I=1,ND)
+          WRITE(LUOUT,'(1X,A,/,(1P,1X,9E14.6))')'CHIL',(CHIL(I),I=1,ND)
+	END IF
+	CLOSE(UNIT=LUOUT)
+!
+	RETURN
+	END
